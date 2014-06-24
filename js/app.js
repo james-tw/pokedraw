c1265e41cb7251edc0f2c959710730fd4f82e441
James TW <meroinx@gmail.com>
2014-06-24 17:18:12 -0400

    Initial commit.

---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+var color = $('.selected').css('background-color');
+var context = $('canvas')[0].getContext('2d');
+
+$('.controls').on('click', 'li', function(){
+  $(this).siblings().removeClass('selected');
+  $(this).addClass('selected');
+  color = $(this).css('background-color');
+});
+
+function changeColor() {
+  var r = $('#red').val();
+  var g = $('#green').val();
+  var b = $('#blue').val();
+  
+  $('#newColor').css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')');
+}
+                     
+$('input[type=range]').change(changeColor);
+
+$('#addNewColor').click(function() {
+  var $newColor = $('<li></li>');
+  $newColor.css('background-color', $('#newColor').css('background-color') );
+  $('.controls ul').append($newColor);
+  $newColor.click();
+});
+
+var $canvas = $('canvas');
+var lastEvent;
+var mouseDown = false;
+
+$canvas.mousedown(function(e){
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e){
+  if(mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.lineWidth = 4;
+    context.lineJoin = 'round';
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mouseDown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
+});
+
+
