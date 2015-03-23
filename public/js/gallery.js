f5d48d870d90a52f208b42c5f4a06a524e106657
James TW <meroinx@gmail.com>
2015-03-23 11:27:25 -0400

    Gallery images improved and saveable
    Changed gallery images to background-images, so they no longer break the grid format. They are also now saveable and display the Pokemon's name on hover.

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -15,7 +15,12 @@ function initializeGallery() {
 function displayNewDrawings() {
   loadPortion = imgList.slice(loadStart, loadStart+40);
   loadPortion.forEach(function(val) {
-    $(".gallery").append($("<div class='drawing'><img src=drawings/" + val._id + "></img></div>"));
+    var capitalizedName = val.pokemon.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+        return letter.toUpperCase();
+    });
+    //Set the background image of the drawing, add the Pokemon name to the drawing, and add a link to download the drawing.
+    var $drawing = $("<div class='drawing'><h1>" + capitalizedName + "</h1><a href='drawings/" + val._id + "' download='" + capitalizedName + ".jpeg'><span class='icon-disk'></span></a></div>").css('background-image', 'url(drawings/' + val._id + ')');
+    $(".gallery").append($drawing);
   });
   $("#loader-button-container").append($('<button class="loader">Load more Pokedraws!</button>'));
   $('.loader').on('click', function(){
@@ -23,16 +28,16 @@ function displayNewDrawings() {
     $(this).remove();
     //Load new images starting with the previous loadStart+40
     displayNewDrawings();
-  })
+  });
   loadStart += 40;
 
 }
 
-  $(window).scroll(function() {
-     if($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
-        $('.loader').remove();
-         displayNewDrawings();
-     }
-  });
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
+    $('.loader').remove();
+    displayNewDrawings();
+  }
+});
 
 initializeGallery();
